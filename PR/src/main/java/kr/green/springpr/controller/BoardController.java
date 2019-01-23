56d677884d5f515f8b8effd03f61d4af610b7927
Flutterby <xikar@naver.com>
2019-01-23 16:02:17 +0900

    Board

--- PR/src/main/java/kr/green/springpr/controller/BoardController.java
+++ PR/src/main/java/kr/green/springpr/controller/BoardController.java
@@ -1,13 +1,14 @@
 package kr.green.springpr.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
 	
 	@RequestMapping(value = "/bbs/list")
-	public String list() {
+	public String list(Model model) {
 		
 		return "bbs/list";
 	}	
---
+++ PR/src/main/java/kr/green/springpr/service/BoardServiceImp.java
@@ -0,0 +1,8 @@
+package kr.green.springpr.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceImp implements BoardService {
+
+}
