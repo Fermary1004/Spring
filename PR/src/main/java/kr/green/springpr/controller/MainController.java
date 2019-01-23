57701b46d77db5a785f55852387618a126370ab3
Flutterby <xikar@naver.com>
2019-01-23 10:33:42 +0900

    Security set

--- PR/src/main/java/kr/green/springpr/controller/MainController.java
+++ PR/src/main/java/kr/green/springpr/controller/MainController.java
@@ -1,12 +1,16 @@
 package kr.green.springpr.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet() {
